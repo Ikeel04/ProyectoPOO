9611afca3ce97d9284cbe3b886e75e89efbf2997
AngelEsquit <esq23221@uvg.edu.gt>
2023-07-21 08:38:50 -0600

    Nuevo archivo

---
+++ Helloworld.java
@@ -0,0 +1,14 @@
+/*
+Universidad del Valle de Guatemala
+cc2008 - POO
+Seccion 10
+
+Pimer programa, Hello World
+07.07.23
+*/
+
+public class Helloworld {
+    public static void main(String args[]) {
+        System.out.println("Hello World");
+    }
+}
